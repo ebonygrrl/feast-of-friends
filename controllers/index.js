592025c46e0b7adc5d84dda95e56b33f40c2e858
flgaitan <fiama_gaitan92@hotmail.com>
2023-01-17 21:25:23 -0600

    changes to index's on controller and api routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,14 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
-const dishRoutes = require('./api/dish-routes');
-const eventRoutes = require('./api/event-routes');
-const dashboardRoutes = require('./api/dashboard-routes');
+
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/dish', dishRoutes);
-router.use('/event', eventRoutes);
-router.use('/dashboard', dashboardRoutes);
+
 
 module.exports = router;
