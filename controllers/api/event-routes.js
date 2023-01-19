1a8a53dcc0f975c9af18048e953fc05e5269a6bc
iwirsing <ivymolina@gmail.com>
2023-01-19 17:52:17 -0600

    merge

--- controllers/api/event-routes.js
+++ controllers/api/event-routes.js
@@ -24,4 +24,27 @@ router.post('/', withAuth,(req,res)=>{
   }
 
 })
+
+//edit event
+router.post('/', withAuth,(req,res)=>{
+  try{
+   //check
+   console.log('line 50 at event-route');
+
+   //create new Event
+   Event.create({
+       theme: req.body.theme,
+       eventDate: req.body.eventDate,
+       where: req.body.location,
+       organizer: req.session.userID,
+      
+   }).then(data=>{
+       // const  event= data.get({plain:true});
+       res.status(200).json(data);
+     })
+  } catch (err) {
+   res.status(400).json(err);
+ }
+
+})
 module.exports = router;
