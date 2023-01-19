08b1caea4198f254aadf70ae43c9a759ac8e1786
iwirsing <ivymolina@gmail.com>
2023-01-19 17:52:17 -0600

    merge

--- controllers/api/event-routes.js
+++ controllers/api/event-routes.js
@@ -4,24 +4,24 @@ const withAuth = require('../../utils/auth');
 
 //create event
 router.post('/', withAuth,(req,res)=>{
-   try{
-    //check
-    console.log('line 50 at event-route');
+  try{
+   //check
+   console.log('line 50 at event-route');
 
-    //create new Event
-    Event.create({
-        theme: req.body.theme,
-        eventDate: req.body.eventDate,
-        where: req.body.location,
-        organizer: req.session.userID,
-       
-    }).then(data=>{
-        // const  event= data.get({plain:true});
-        res.status(200).json(data);
-      })
-   } catch (err) {
-    res.status(400).json(err);
-  }
+   //create new Event
+   Event.create({
+       theme: req.body.theme,
+       eventDate: req.body.eventDate,
+       where: req.body.location,
+       organizer: req.session.userID,
+      
+   }).then(data=>{
+       // const  event= data.get({plain:true});
+       res.status(200).json(data);
+     })
+  } catch (err) {
+   res.status(400).json(err);
+ }
 
 })
 
@@ -45,7 +45,10 @@ router.put('/', withAuth,(req,res)=>{
    res.status(500).json(err);
  }
 
-})
+});
+
+
+
 
 //delete event
 router.delete('/',withAuth,(req,res)=>{
