24f3f13e58f8e772a4d2b9cbe7ac4c77bf118f88
iwirsing <ivymolina@gmail.com>
2023-01-20 13:47:27 -0600

    working crud

--- public/js/event.js
+++ public/js/event.js
@@ -4,17 +4,62 @@ const potluckID=document.querySelector('#event-id').innerHTML;
 //edit potluck 
 const editPotluckFormHandler = async (event) => {
     event.preventDefault();
-
-
     // console.log(potluckID);
     //CHECK
-    console.log('line 7 in event.js');
+    console.log('line 9 in event.js');
     //navigate to create Potluck Page
     document.location.replace(`/edit/${potluckID}`);
 };
   
 const signupBtn=document.getElementById("editEvent");
 signupBtn.addEventListener("click", editPotluckFormHandler);
+
+//go back to dashboard
+const toDashboardFormHandler = (event) => {
+    event.preventDefault();
+    // console.log(potluckID);
+    //CHECK
+    console.log('line 22 in event.js');
+    //navigate to create Potluck Page
+    document.location.replace(`/dashboard`);
+};
+  
+const toDashBtn=document.getElementById("backDashboard");
+toDashBtn.addEventListener("click", toDashboardFormHandler);
+
+//delete potluck
+const deletePotluckFormHandler = async (event) => {
+    event.preventDefault();
+
+    //ask if user is sure to delete event
+    let deletePrompt=confirm('Are you sure you want to delete this potluck?');
+    console.log('line 36 in event.js', deletePrompt, potluckID);
+
+    if(potluckID && deletePrompt){
+        const response = await fetch(`/api/event/`,{
+          method: 'DELETE',
+          body: JSON.stringify({ potluck_ID: potluckID }),
+          headers: { 'Content-Type': 'application/json' },
+        }).then((data)=>data.json()).then((info)=>{
+          
+          console.log('line 41 event.js',info);
+          //after delete navigate to dashboard
+          document.location.replace(`/dashboard`);
+        }).catch((err)=>alert('Failed to delete potluck.', err));
+    } 
+    // else{
+    //     //go back to potluck event
+    //     // document.location.replace(`/event/${potluckID}`);
+
+    // };
+};
+
+
+
+const delBtn= document.getElementById("delEvent");
+delBtn.addEventListener("click", deletePotluckFormHandler);
+
+
 
 
 
